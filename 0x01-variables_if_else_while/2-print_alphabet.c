258fc7c8c45352e27158dae0f0ecad1f57f1d51d
christophergachie <christophergachie5@gmail.com>
2023-09-08 12:22:47 -0700

    tit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 int i;
-char alphabet [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphabet [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char result;
 for (i=0; i<=25; i++)
 {
 result = tolower(alphabet[i]);
 putchar(result);
-putchar("\n");
+putchar('\n');
 }
 return (0);
 }
