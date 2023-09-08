99e74a30059a3c50c34221cf1141b02ce8e60347
christophergachie <christophergachie5@gmail.com>
2023-09-08 16:51:58 -0700

    hih

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,21 +2,21 @@
 #include <string.h>
 /**
 * main - Entry point
-* 
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 char hexa;
-for (hexa =0; hexa <0x10; hexa++)
+for (hexa = 0; hexa < 0x10; hexa++)
 {
-if(hexa < 10)
+if (hexa < 10)
 {
 putchar(hexa + '0');
 }
 else
 {
-putchar(hexa -10 + 'a');
+putchar(hexa - 10 + 'a');
 }
 }
 putchar('\n');
