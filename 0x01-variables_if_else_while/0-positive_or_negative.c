e67922720d07c1bf532e78abad5eadb715228dd4
christophergachie <christophergachie5@gmail.com>
2023-09-08 10:24:18 -0700

    jij

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - start of the program
  *
- * return - returns 0
+ * Return - returns 0 at the end of the program
  *
  */
 int main(void)
@@ -18,11 +18,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative\n",n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
