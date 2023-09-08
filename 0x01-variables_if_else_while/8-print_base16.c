4155ed06ced4d608e7e662f0782551b169137539
christophergachie <christophergachie5@gmail.com>
2023-09-08 16:44:43 -0700

    kiik

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char hexa;
+for (hexa =0; hexa <0x10; hexa++)
+{
+if(hexa < 10)
+{
+putchar(hexa + '0');
+}
+else
+{
+putchar(hexa -10 + 'a');
+}
+putchar('\n');
+return (0);
+}
