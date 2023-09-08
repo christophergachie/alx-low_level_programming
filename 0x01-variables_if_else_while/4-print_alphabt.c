e003062c5c31ecb99568dc2999a84a6c6ccabfac
christophergachie <christophergachie5@gmail.com>
2023-09-08 13:51:51 -0700

    tit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch == 'e' || ch == 'q')
+{
+continue;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
