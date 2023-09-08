cff351309190f4bfe63915439f1144c2c4ce9161
christophergachie <christophergachie5@gmail.com>
2023-09-08 12:44:04 -0700

    kik

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 int i;
-char alphabet [] = {'a', 'b', 'c', 'd', 'e', 'f',
-'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f',
+'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 char result;
 for (i = 0; i <= 25; i++)
 {
