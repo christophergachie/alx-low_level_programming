e29e38fdb5ecdb531ce3ef3eeee1b11f963b4bca
christophergachie <christophergachie5@gmail.com>
2023-09-09 00:41:48 -0700

    kik

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int first;
+int last;
+for(first = 0; first <= 9; first++)
+{
+for(last = 0; last <= 9; last++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 9 || j!= 9)
+{
+putchar(',');
+}
+}
+}
+putchar('\n');
+rgeturn (0);
+}
