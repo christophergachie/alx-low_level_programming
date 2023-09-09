f8c6a5e96d4a7353fb984fd1af7ea64842192636
christophergachie <christophergachie5@gmail.com>
2023-09-09 00:55:42 -0700

    hihi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,6 +4,8 @@
 *
 * Return: Always 0 (Success)
 */
+int main(void)
+{
 int first;
 int last;
 for (first = 0; first <= 9; first++)
@@ -12,7 +14,7 @@ for (last = 0; last <= 9; last++)
 {
 putchar(first + '0');
 putchar(last + '0');
-if (i != 9 || j!= 9)
+if (first != 9 || last != 9)
 {
 putchar(',');
 }
