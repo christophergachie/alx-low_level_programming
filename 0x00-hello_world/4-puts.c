afee66b888e0446c40f296da927dd96bad8a2693
christophergachie <christophergachie5@gmail.com>
2023-09-10 16:26:41 -0700

    oio

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (Successs)
+*/
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
