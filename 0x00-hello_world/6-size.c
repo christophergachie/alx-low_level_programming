07e1d807f889ab88d6a55ced96287b7dff28a1a2
christophergachie <christophergachie5@gmail.com>
2023-09-10 16:58:46 -0700

    tit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main()
 {
 printf("Size of a char: %d byte(s)\n", 1);
 printf("Size of an int: %d byte(s)\n", 4);
-printf("Size of a long int: %d byte(s)\n", 4);
+printf("Size of a long int: %d byte(s)\n", 8);
 printf("Size of a long long int: %d byte(s)\n", 8);
 printf("Size of a float: %d byte(s)\n", 4);
 return (0);
