1bda878cc877b88877590a3c228fee4976a4d340
christophergachie <christophergachie5@gmail.com>
2023-09-12 12:58:14 -0700

    tit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,6 @@
 * Return: On success, returns the character written. On error, returns -1.
 */
 void print_alphabet(void);
-int _putchar(char c)
-{
-return write(1, &c, 1);
-}
 void print_alphabet(void)
 {
 int i;
