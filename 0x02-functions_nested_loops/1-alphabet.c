11105b97cf68bca5801595d3ca7ae3547676a8f8
christophergachie <christophergachie5@gmail.com>
2023-09-12 12:19:07 -0700

    tit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@
 void print_alphabet(void);
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+return write(1, &c, 1);
 }
 void print_alphabet(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 int _putchar(char c)
 {
-return(write(1, &c, 1));
+return write(1, &c, 1);
 }
 void print_alphabet(void);
