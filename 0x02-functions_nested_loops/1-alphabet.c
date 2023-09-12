f588f0e702d5d5a133419b8d11c54130d7bfc51d
christophergachie <christophergachie5@gmail.com>
2023-09-12 12:11:15 -0700

    tit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+/**
+* main -Entry point of the program
+* @c - outputs a single character to the screen
+* Description: This function writes the character 'c' to the standard output.
+*             It is commonly used to print individual characters.
+*
+* Return: On success, returns the character written. On error, returns -1.
+*/
+void print_alphabet(void);
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <=  'z'; i++)
+{
+_putchar(i);
+}
+}
+int main(void)
+{
+print_alphabet();
+_putchar('\n');
+return (0);
+}
