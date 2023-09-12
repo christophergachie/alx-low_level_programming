6328c7c344e8d81e009d2815f29b8204195cbc82
christophergachie <christophergachie5@gmail.com>
2023-09-12 14:26:24 -0700

    kik

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,13 @@
 #include <unistd.h>
+#include "main.h"
 /**
 * main -Entry point of the program
 * @c - outputs a single character to the screen
 * Description: This function writes the character 'c' to the standard output.
 *             It is commonly used to print individual characters.
 *
-* Return: On success, returns the character written. On error, returns -1.
+* Return: On success, returns the character written. On error, returns -1,
 */
-int _putchar(char c)
-{
-return write(1, &c, 1);
-}
-void print_alphabet_x10(void);
 void print_alphabet_x10(void){
 int i, j;
 for (j = 0; j < 10; j++)
@@ -23,8 +19,4 @@ _putchar(i);
 _putchar('\n');
 }
 }
-int main(void)
-{
-print_alphabet_x10();
-return (0);
 }
