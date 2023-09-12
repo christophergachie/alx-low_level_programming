2492f7f8626624a51218a917dc3aa52cc7654903
christophergachie <christophergachie5@gmail.com>
2023-09-12 13:10:47 -0700

    kik

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,4 @@
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+#include <unistd.h>
 void print_alphabet(void);
 int _putchar(char c)
 {
