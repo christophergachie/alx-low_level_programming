dc09abb26f95714b1dcad13cbb1303a7f1270c60
christophergachie <christophergachie5@gmail.com>
2023-09-12 11:18:30 -0700

    tit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/**
+* main - entry point of the program
+*_putchar- otuputs single char to the screen
+* Return: Always (0) Success
+*
+*/
+int _putchar(char c);
+int main (void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
+}
