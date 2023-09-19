bb1583f0db8565b1ab7ab6bd5ff64f07afc8b8f0
christophergachie <christophergachie5@gmail.com>
2023-09-19 11:11:01 -0700

    tit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+/**
+* _strlen - Calculates the length of a string.
+* @s: The string to calculate the length of.
+*
+* Return: The length of the string.
+*/
 int _strlen(char *s)
 {
 int len;
