470428e298e53a15afd41489649b4ffb8a786589
christophergachie <christophergachie5@gmail.com>
2023-09-19 08:50:29 -0700

    kik

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* reset_to_98 - Sets an integer to 98.
-*
-* swap_int - this function swaps the values of two integers.
+* swap_int - swaps the values of two integers.
 * @a: Pointer to the first integer that will be swapped
 * @b: Pointer to the second integer that will be swapped
 *
 * Description: This function takes two pointers to integers, 'a' and 'b', and swaps
 * their values. The value pointed to by 'a' will be assigned to 'b', and vice versa.
-* main - check the code
 *
 * Return: Always 0.
-* and sets the value of the integer to 98.
 */
 void swap_int(int *a, int *b)
 {
