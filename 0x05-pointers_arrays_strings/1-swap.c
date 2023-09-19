7be80dab66e7a7c0220f8c6bc3f5d4a4ceed95b8
christophergachie <christophergachie5@gmail.com>
2023-09-19 08:37:43 -0700

    tit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - Sets an integer to 98.
+* @n: A pointer to an integer.
+*
+* Description: This function takes a pointer to an integer as a parameter
+* main - check the code
+*
+* Return: Always 0.
+* and sets the value of the integer to 98.
+*/
+void swap_int(int *a, int *b)
+{
+pointer_swap = *a;
+*a = *b;
+*b = pointer;
+}
