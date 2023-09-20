534452943b2323bdd432b5424b9acc92d4bdf502
christophergachie <christophergachie5@gmail.com>
2023-09-19 18:50:57 -0700

    ij

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* rev_string - Reverses a string
+* @s: Input string
+* Return: void
+*/
+void rev_string(char *s)
+{
+char rev = s[0];
+int reverse_it = 0;
+int a;
+for (; s[reverse_it] != '\0'; reverse_it++)
+;
+for (a = 0; a < reverse_it; a++)
+{
+reverse_it--;
+rev = s[a];
+s[a] = s[reverse_it];
+s[counter] = rev;
+}
+}
