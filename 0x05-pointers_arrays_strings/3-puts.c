ed92b2df65c4f05695a425d05bfc28f6db35f980
christophergachie <christophergachie5@gmail.com>
2023-09-19 17:45:42 -0700

    kik

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: returns the output of the function _strlen
+*/
+int _strlen(char *s)
+{
+int string_length = 0;
+for (; *s != '\0'; s++)
+{
+string_length++;
+}
+return(string_length);
+}
