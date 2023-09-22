f222ae58d9dea21c1aa5c7e9130c4a92d5719aa3
christophergachie <christophergachie5@gmail.com>
2023-09-22 00:25:58 -0700

    kik

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
 * main -Entry point of the program
-* @c - outputs a single character to the screen
-* Description: This function writes the character 'c' to the standard output.
 * print_alphabet - print all alphabet in lowercase
 *
 * Return: On success, returns the character written. On error, returns -1.
