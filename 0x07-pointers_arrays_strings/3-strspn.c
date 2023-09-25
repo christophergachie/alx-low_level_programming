b56a48ba5bca2d7502e1bc41824eaaa35ae52c1b
christophergachie <christophergachie5@gmail.com>
2023-09-25 15:09:43 -0700

    iuiiui

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strspn - Entry point
+* @s: input
+* @accept: input
+* Return: Always 0 (Success)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, n, value, check;
+value = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (accept[n] == s[i])
+value++;
+check = 1;
+}
+}
