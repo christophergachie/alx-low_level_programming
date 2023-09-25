3b547cb518a21a3af29339e5bd04204c40c443d1
christophergachie <christophergachie5@gmail.com>
2023-09-25 13:25:26 -0700

    tit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* simple_print_buffer - prints buffer in hexa
-* @buffer: the address of memory to print
-* @size: the size of the memory to print
-*
-* Return: Nothing.
+* _memeset - Entry point
+* @s: pointed destination
+* @b: constant byte
+* @n: bytes
+* Return: Always 0 (Success)
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-for (i = 0; i < n; i++;
+for (i = 0; i < n; i++;)
 {
 s[i] = b;
 }
