ea13573250d777bfa8b516dcfbed8cddebe45960
christophergachie <christophergachie5@gmail.com>
2023-09-25 15:18:35 -0700

    kiik

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,14 @@ unsigned int i, n, value, check;
 value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
+check = 0;
+for (n =0; accept[n] != '\0'; n++)
+{
 if (accept[n] == s[i])
+{
 value++;
 check = 1;
+}
+}
 }
 }
