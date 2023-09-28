1af974fb561fabc948cf41dce9a6ce8dae47bbd2
christophergachie <christophergachie5@gmail.com>
2023-09-28 12:20:14 -0700

    tyt

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,12 @@
 #include "main.h"
 /**
 * _isalpha -Entry point
-* @C accomodates uppercase characters
-* @c fucntion argument checked if true
+*Check if a character is an alphabet character.
+* 
+* @param c The character to be checked.
+* @return 1 if the character is an alphabet character 
+* ('a' to 'z' or 'A' to 'Z'),
+* 0 otherwise.
 * Return 1 if c is a character
 * and 0 if c is a non-character
 */
